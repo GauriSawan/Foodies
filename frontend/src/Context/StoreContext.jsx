06a5c0c553cb3728c54d8ab6782c405ce316df42
Gauri Sawant <gaurisawantas99@gmail.com>
2025-07-15 18:29:41 +0400

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://foodies-eja6.onrender.com/"
+    const url = "https://foodies-eja6.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
